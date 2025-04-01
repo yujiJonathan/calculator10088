32075e9c024b32268424e8092fe76fc43f983868
yuji0305 <jonathan.futagawa.yuji@pathoslogos.co.jp>
2025-04-01 18:59:23 +0900

    [ADD]コンポーネント連携　app calculator

--- calculator/src/app/app.component.ts
+++ calculator/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalculatorComponent } from '../app/calculator/calculator.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CalculatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
